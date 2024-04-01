33575b9a6f63d8dad740ca4c8524c1320ee0441a
ytakahashi2020 <y.takahashi2020@gmail.com>
2024-04-02 07:52:11 +0900

    first commit

---
+++ client.ts
@@ -0,0 +1,54 @@
+import {
+  Connection,
+  Keypair,
+  SystemProgram,
+  Transaction,
+  clusterApiUrl,
+  sendAndConfirmTransaction,
+  ComputeBudgetProgram,
+} from "@solana/web3.js";
+
+// Playground wallet
+const payer = pg.wallet.keypair;
+
+// Connection to devnet cluster
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+// Generate new keypair for Mint Account
+const toAccountKeypair = Keypair.generate();
+// Address for Mint Account
+const toAccount = toAccountKeypair.publicKey;
+
+// Transaction signature returned from sent transaction
+let transactionSignature: string;
+
+const modifyComputeUnits = ComputeBudgetProgram.setComputeUnitLimit({
+  units: 450,
+});
+
+const addPriorityFee = ComputeBudgetProgram.setComputeUnitPrice({
+  microLamports: 1200000,
+});
+
+const transaction = new Transaction()
+  .add(addPriorityFee)
+  .add(modifyComputeUnits)
+  .add(
+    SystemProgram.transfer({
+      fromPubkey: payer.publicKey,
+      toPubkey: toAccount,
+      lamports: 10000000,
+    })
+  );
+
+// Send transaction
+transactionSignature = await sendAndConfirmTransaction(
+  connection,
+  transaction,
+  [payer] // Signers
+);
+
+console.log(
+  "\nCreate Mint Account:",
+  `https://solana.fm/tx/${transactionSignature}?cluster=devnet-solana`
+);
